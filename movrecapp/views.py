f493b70ebd5d81ee2689321fd1854f97ea9e231f
qor-101 <qor6119@gmail.com>
2021-07-28 22:31:53 +0530

    Improved Randomization

--- movrecapp/views.py
+++ movrecapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from imdb import IMDb
+import random
 # Create your views here.
 
 def index(request):
@@ -11,8 +12,8 @@ def index(request):
 def getmov(request):
     ia = IMDb()
     # get a movie
-    movie = ia.search_movie('matrix')
+    movie = ia.get_movie(random.randint(1000000,5000000))
 
-    mov_url = ia.get_imdbURL(movie[0])
+    mov_url = ia.get_imdbURL(movie)
 
     return redirect(mov_url) 
--- movsite/settings.py
+++ movsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movrecapp.apps.MovrecappConfig'
+    'movrecapp.apps.MovrecappConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'movrecapp/static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'movrecapp/assets')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
